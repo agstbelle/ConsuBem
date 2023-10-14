1e36097e863fd438a68593e45ce86e1aee4a8e9b
Isabelle <isabellepontes91@gmail.com>
2023-10-13 21:50:35 -0300

    urls, views, models e função cadastro

--- app_consubem/models.py
+++ app_consubem/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.TextField(max_length=255)
+    telefone = models.CharField(max_length = 11)
+    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
--- app_consubem/views.py
+++ app_consubem/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import Usuario
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Create your views here.
-def index (request):
-    return render (request, 'index.html')
+def index(request):
+    return render(request, 'index.html')
 
-def cadastro (request):
-    return render (request, 'cadastro.html')
+def cadastro(request):
+    if request.POST:
+        senha = request.POST.get('senha')
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+        usuario = Usuario.objects.filter(user__email=email).first()
 
-def login (request):
-    return render (request, 'login.html')
+        if usuario is None:
+  
+            user = User.objects.create_user(username=email, email=email, password=senha)
+            user.save()
+            usuario = Usuario(nome=nome, telefone=telefone, user=user)
+            usuario.save()
+            messages.success(request, 'Cadastro realizado com sucesso.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Usuário já cadastrado.')
+            return render(request, 'cadastro.html')
+    return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
+ 
+
+
+
+
+
 
